53f9e009547addf99d9a1c97bb1a4a1fa1edf713
Anthony McLamb <adylanmclamb@yahoo.com>
2023-12-01 01:33:04 -0500

    fix auth0

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 import "./globals.css";
 // font awesome
 import { config, dom } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { UserProvider } from "@auth0/nextjs-auth0/client";
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
