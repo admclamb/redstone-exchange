8b7e90906431fb3e567eabc9ff49edada7aff3b7
Anthony McLamb <adylanmclamb@yahoo.com>
2023-12-01 01:31:56 -0500

    testing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,7 @@
-import Navbar from "@/components/navbar/Navbar";
-
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <header>
-        <Navbar />
-      </header>
+      <header></header>
       <main className="flex min-h-screen flex-col">
         <p>PEEOE</p>
       </main>
