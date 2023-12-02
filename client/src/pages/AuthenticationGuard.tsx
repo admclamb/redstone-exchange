296cefba3d21ddeb844e338fa23a3ac5c16b3118
Anthony McLamb <adylanmclamb@yahoo.com>
2023-12-02 00:50:50 -0500

    fix for prod

--- client/src/pages/AuthenticationGuard.tsx
+++ client/src/pages/AuthenticationGuard.tsx
@@ -1,5 +1,6 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import PageLoader from "../components/loader/page-loader/PageLoader";
+import { ComponentType } from "react";
 
 interface AuthenticationGuardProps {
   component: ComponentType;
